964884b974da7c8217dc39f4687dda88f2d3d40c
Woosung_Kim <sibu2005@naver.com>
2023-10-20 17:43:50 +0900

    spring batch project init

---
+++ src/test/java/io/springbatchexample/SpringBatchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
