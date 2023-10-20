9608e2afec461d9597b90512dc619889b49db39e
Woosung_Kim <sibu2005@naver.com>
2023-10-20 17:44:32 +0900

    feat : batch 설정 분리

---
+++ src/main/java/io/springbatchexample/config/BatchConfig.java
@@ -0,0 +1,9 @@
+package io.springbatchexample.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBatchProcessing
+public class BatchConfig {
+}
