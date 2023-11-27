490ee42e46b9a85bbd808e4896b0cdda2db66119
Woosung_Kim <sibu2005@naver.com>
2023-11-27 21:42:22 +0900

    Batch JobRepository 커스텀 설정

---
+++ src/main/java/io/springbatchexample/config/basic/config/CustomBatchConfigurer.java
@@ -0,0 +1,34 @@
+package io.springbatchexample.config.basic.config;
+
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * BATCH 초기화시 JobRepository를 커스터마이징해서 등록함
+ */
+@Configuration
+public class CustomBatchConfigurer extends BasicBatchConfigurer {
+    private final DataSource dataSource;
+
+    protected CustomBatchConfigurer(BatchProperties properties, DataSource dataSource, TransactionManagerCustomizers transactionManagerCustomizers, DataSource dataSource1) {
+        super(properties, dataSource, transactionManagerCustomizers);
+        this.dataSource = dataSource1;
+    }
+
+    @Override
+    protected JobRepository createJobRepository() throws Exception {
+
+        JobRepositoryFactoryBean factoryBean = new JobRepositoryFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTransactionManager(getTransactionManager());
+        factoryBean.setIsolationLevelForCreate("ISOLATION_READ_COMMITTED");
+        factoryBean.setTablePrefix("WOO_");
+        return factoryBean.getObject();
+    }
+}
