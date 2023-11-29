ac9c94ea51b829bb2244e37ec85c5f25e4097b6e
Woosung_Kim <sibu2005@naver.com>
2023-11-29 23:25:10 +0900

    basic & training 같이 진행, 구성 변경

--- src/main/java/io/springbatchexample/config/BatchConfig.java
+++
@@ -1,9 +0,0 @@
-package io.springbatchexample.config;
-
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableBatchProcessing
-public class BatchConfig {
-}
