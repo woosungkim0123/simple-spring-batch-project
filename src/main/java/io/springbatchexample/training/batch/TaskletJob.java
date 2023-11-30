2a9b797fd606e3a1229e5299cb3f4d628991b56c
Woosung_Kim <sibu2005@naver.com>
2023-11-30 20:21:25 +0900

    training - 기본 tasklet

--- src/main/java/io/springbatchexample/training/batch/TaskletJob.java
+++ src/main/java/io/springbatchexample/training/batch/TaskletJob.java
@@ -1,4 +1,4 @@
-package io.springbatchexample.training;
+package io.springbatchexample.training.batch;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
---
+++ src/test/java/io/springbatchexample/domain/TestDeptRepository.java
@@ -0,0 +1,25 @@
+package io.springbatchexample.domain;
+
+import io.springbatchexample.training.domain.Dept1;
+import io.springbatchexample.training.domain.Dept1Repository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+@SpringBootTest
+public class TestDeptRepository {
+
+    @Autowired
+    private Dept1Repository dept1Repository;
+
+    @Commit
+    @DisplayName("training을 위한 테스트 데이터를 만든다.")
+    @Test
+    void depth01() {
+        for (int i = 0; i < 101; i++) {
+            dept1Repository.save(new Dept1((long) i, "name_" + i, "location_" + i));
+        }
+    }
+}
